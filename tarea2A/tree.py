a5294cdd72ff639cc30494c8904e80fbe84b18f7
alec280 <alec280@gmail.com>
2020-05-04 23:45:32 -0400

    Bases de la tarea 2A, nada remarcable.

---
+++ tarea2A/tree.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+"""
+Alexander Cuevas, CC3501, 2020-1
+Work in progress, will become a tree
+"""
+
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+import sys
+
+import transformations as tr
+import basic_shapes as bs
+import easy_shaders as es
+import scene_graph as sg
+import lighting_shaders as ls
+
+
+# A class to store the application control
+class Controller:
+    def __init__(self):
+        self.fillPolygon = True
+
+
+# We will use the global controller as communication with the callback function
+controller = Controller()
+
+def on_key(window, key, scancode, action, mods):
+
+    if action != glfw.PRESS:
+        return
+    
+    global controller
+
+    if key == glfw.KEY_SPACE:
+        controller.fillPolygon = not controller.fillPolygon
+
+    elif key == glfw.KEY_ESCAPE:
+        sys.exit()
+
+
+if __name__ == "__main__":
+
+    # Initialize glfw
+    if not glfw.init():
+        sys.exit()
+
+    width = 600
+    height = 600
+
+    window = glfw.create_window(width, height, "Invisible tree", None, None)
+
+    if not window:
+        glfw.terminate()
+        sys.exit()
+
+    glfw.make_context_current(window)
+
+    # Connecting the callback function 'on_key' to handle keyboard events
+    glfw.set_key_callback(window, on_key)
+
+    # Setting up the clear screen color
+    glClearColor(0.85, 0.85, 0.85, 1.0)
+
+    # As we work in 3D, we need to check which part is in front,
+    # and which one is at the back
+    glEnable(GL_DEPTH_TEST)
+
+    glfw.terminate()
