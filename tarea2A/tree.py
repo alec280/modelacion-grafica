42ee27f26b005a7da8375aa954a05eb6b8e044e7
alec280 <alec280@gmail.com>
2020-05-06 14:59:17 -0400

    Los árboles ahora tienen hojas.

--- tarea2A/tree.py
+++ tarea2A/tree.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """
 Alexander Cuevas, CC3501, 2020-1
-Work in progress, will become a tree
+Creates a low poly tree
 """
 
 import glfw
@@ -15,6 +15,10 @@ import basic_shapes as bs
 import easy_shaders as es
 import scene_graph as sg
 import lighting_shaders as ls
+
+
+# Constant used in the creation of brances
+BRANCH_ANGLE = 27 * np.pi / 180
 
 
 # Processing the parameters and name given for the .obj model
@@ -76,6 +80,52 @@ def on_key(window, key, scancode, action, mods):
         sys.exit()
 
 
+# Creates a regular dodecahedron, the green channel can be modified
+def createDodecahedron(g = 0.4):
+
+    # The golden ratio and its inverse
+    gRa = (1 + np.sqrt(5)) / 2
+    iRa = 1 / gRa
+
+    # Defining the location and colors of each vertex  of the shape
+    vertices = [
+            #    positions        colors          normals
+             iRa,  gRa,  0.0, 0.0, g, 0.0,  iRa,  gRa,  0.0,
+             gRa,  0.0,  iRa, 0.0, g, 0.0,  gRa,  0.0,  iRa,
+             1.0,  1.0,  1.0, 0.0, g, 0.0,  1.0,  1.0,  1.0,
+             gRa,  0.0, -iRa, 0.0, g, 0.0,  gRa,  0.0, -iRa,
+             1.0,  1.0, -1.0, 0.0, g, 0.0,  1.0,  1.0, -1.0,
+             1.0, -1.0,  1.0, 0.0, g, 0.0,  1.0, -1.0,  1.0,
+             iRa, -gRa,  0.0, 0.0, g, 0.0,  iRa, -gRa,  0.0,
+             1.0, -1.0, -1.0, 0.0, g, 0.0,  1.0, -1.0, -1.0,
+             0.0, -iRa, -gRa, 0.0, g, 0.0,  0.0, -iRa, -gRa,
+             0.0,  iRa, -gRa, 0.0, g, 0.0,  0.0,  iRa, -gRa,
+            -1.0, -1.0, -1.0, 0.0, g, 0.0, -1.0, -1.0, -1.0,
+            -iRa, -gRa,  0.0, 0.0, g, 0.0, -iRa, -gRa,  0.0,
+            -1.0,  1.0, -1.0, 0.0, g, 0.0, -1.0,  1.0, -1.0,
+            -gRa,  0.0, -iRa, 0.0, g, 0.0, -gRa,  0.0, -iRa,
+            -iRa,  gRa,  0.0, 0.0, g, 0.0, -iRa,  gRa,  0.0,
+             0.0,  iRa,  gRa, 0.0, g, 0.0,  0.0,  iRa,  gRa,
+            -1.0,  1.0,  1.0, 0.0, g, 0.0,  1.0,  1.0, -1.0,
+            -1.0, -1.0,  1.0, 0.0, g, 0.0, -1.0, -1.0,  1.0,
+            -gRa,  0.0,  iRa, 0.0, g, 0.0, -gRa,  0.0,  iRa,
+             0.0, -iRa,  gRa, 0.0, g, 0.0,  0.0, -iRa,  gRa]
+    
+    # Defining connections among vertices
+    # We have a triangle every 3 indices specified
+    indices = [0,  1,  2,  0,  3,  1,  0,  4,  3,  9,  3,  4,
+               9,  7,  3,  9,  8,  7, 11,  7,  6, 11,  8,  7,
+              11, 10,  8,  6,  1,  5,  6,  3,  1,  6,  7,  3,
+              12,  0, 14, 12,  4,  0, 12,  9,  4, 10, 12, 13,
+              10,  9, 12, 10,  8,  9, 17, 13, 18, 17, 10, 13,
+              17, 11, 10, 16,  2, 15, 16,  0,  2, 16, 14,  0,
+              18, 14, 16, 18, 12, 14, 18, 13, 12, 19,  2, 15,
+              19,  1,  2, 19,  5,  1, 17,  5, 19, 17,  6,  5,
+              17, 11,  6, 18, 15, 16, 18, 19, 15, 18, 17, 19]
+
+    return bs.Shape(vertices, indices)
+
+
 # Creates a branch or tree trunk of variable length
 # Ratio: Size of the bases of the shape in proportion to the lenght
 # Sides: How many sides are used to create the bases of the shape,
@@ -132,13 +182,35 @@ def createBranch(length = 1.0, ratio = 0.1, sides = 6):
     return bs.Shape(vertices, indices)
 
 
+# Creates a dodecahedron of a certain size and green coloration to
+# be used as leaves for a tree, it returns a scene graph
+def createLeaf(size = 0.2, green = 0.4):
+
+    # Scene graph that will contain the scaled leaf
+    scaledLeafGraph = sg.SceneGraphNode("scaledLeaf")
+    scaledLeafGraph.transform = tr.uniformScale(size)
+    scaledLeafGraph.childs += [es.toGPUShape(createDodecahedron(green))]
+
+    # Final scene graph
+    leafGraph = sg.SceneGraphNode("leaf")
+    leafGraph.childs += [scaledLeafGraph]
+
+    return leafGraph
+
+
+
 # Creates a tree using a rule of generation, order of iterations and size
 # There is also decay, which affects the size of branches as their distance
 # with the tree trunk increases
-def createTree(order = 1, rule = "F[RF]F[LF]F", size = 1.0, decay = 0.8):
+def createTree(order = 1, rule = "F[RF]F[LF]F", size = 1.0):
+
+    # The different parts of the tree with different materials
+    woodGraph = sg.SceneGraphNode("wood")
+    leavesGraph = sg.SceneGraphNode("leaf")
 
     # Scene graph that will contain the whole tree
     treeGraph = sg.SceneGraphNode("tree")
+    treeGraph.childs += [woodGraph, leavesGraph]
 
     # String used to construct the tree
     blueprint = "F"
@@ -154,12 +226,14 @@ def createTree(order = 1, rule = "F[RF]F[LF]F", size = 1.0, decay = 0.8):
         
         blueprint = newBlueprint
     
+    blueprint += "]"
+    
     # Counts how many times the rule is applied, reducing the size of the
     # branches in order to make a tree of the given size
     for character in rule:
 
-        if character == "F":
-            counter += 1 - int(lockCounter > 0)
+        if character == "F" and lockCounter == 0:
+            counter += 1
         
         elif character == "[":
             lockCounter += 1
@@ -172,7 +246,7 @@ def createTree(order = 1, rule = "F[RF]F[LF]F", size = 1.0, decay = 0.8):
     # Lists that store the information necessary to put new branches
     phiList = [0]
     thetaList = [0]
-    decayList = [decay]
+    decayList = [0.8]
     xList = [0]
     yList = [0]
     zList = [0]
@@ -182,6 +256,7 @@ def createTree(order = 1, rule = "F[RF]F[LF]F", size = 1.0, decay = 0.8):
     # Creating the tree
     for character in blueprint:
 
+        # Creating a new branch considering the lists of properties
         if character == "F":
 
             phi = phiList[-1]
@@ -191,8 +266,10 @@ def createTree(order = 1, rule = "F[RF]F[LF]F", size = 1.0, decay = 0.8):
             y = yList[-1]
             z = zList[-1]
 
+            # Adding the model to the tree graph
             gpuBranch = es.toGPUShape(createBranch(size, 0.05))
 
+            # Adding the wood
             branchGraph = sg.SceneGraphNode("branch")
             branchGraph.childs += [gpuBranch]
 
@@ -201,33 +278,48 @@ def createTree(order = 1, rule = "F[RF]F[LF]F", size = 1.0, decay = 0.8):
             branchGraph.transform = tr.matmul([rotation, tr.uniformScale(decay)])
             branchGraph.transform = tr.matmul([tr.translate(x, y, z), branchGraph.transform])
 
-            treeGraph.childs += [branchGraph]
+            woodGraph.childs += [branchGraph]
 
+            # Changing the position of the next branch
             localSize = size * decay
 
             xList[-1] += localSize * np.sin(phi) * np.cos(theta)
             yList[-1] += localSize * np.sin(phi) * np.sin(theta)
             zList[-1] += localSize * np.cos(phi)
         
+        # Changing the angle of the next branch using a spherical system
         elif character == "R":
-            phiList[-1] += 27 * np.pi / 180
+            phiList[-1] += BRANCH_ANGLE
 
         elif character == "L":
-            phiList[-1] -= 27 * np.pi / 180
+            phiList[-1] -= BRANCH_ANGLE
         
         elif character == "U":
-            thetaList[-1] += 27 * np.pi / 180
+            thetaList[-1] += BRANCH_ANGLE
         
         elif character == "D":
-            thetaList[-1] -= 27 * np.pi / 180
+            thetaList[-1] -= BRANCH_ANGLE
         
+        # Setting up a new path for the branches
         elif character == "[":
             for ls in allLists:
                 ls.append(ls[-1])
 
             decayList[-1] **= 2
         
+        # Closing a path, sometimes with a leaf
         elif character == "]":
+
+            x = xList[-1]
+            y = yList[-1]
+            z = zList[-1]
+
+            # Adding a leaf at the end of a path
+            leafGraph = createLeaf(0.2 * size * decayList[-1])
+            leafGraph.transform = tr.translate(x, y, z)
+
+            leavesGraph.childs += [leafGraph]
+
             for ls in allLists:
                 ls.pop()
 
@@ -238,7 +330,7 @@ def createTree(order = 1, rule = "F[RF]F[LF]F", size = 1.0, decay = 0.8):
 # returns the view matrix and the viewPos vector for later use
 def moveCamera():
 
-    # Angles of a spheric system, determined by the controller
+    # Angles of a spherical system, determined by the controller
     phi = controller.cameraPhi
     theta = controller.cameraTheta
 
@@ -261,6 +353,22 @@ def moveCamera():
     return tr.lookAt(viewPos, np.array([0, 0, 0.4]), viewUp), viewPos
 
 
+# Draws a given tree using a pipeline
+def drawTree(tree, pipeline):
+
+    # Object is barely visible at only ambient and brighter for the diffuse component.
+    glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ka"), 0.3, 0.3, 0.3)
+    glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Kd"), 0.5, 0.5, 0.5)
+    glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ks"), 0.1, 0.1, 0.1)
+
+    # In my humble opinion, natural wood isn't shiny at all
+    glUniform1ui(glGetUniformLocation(pipeline.shaderProgram, "shininess"), 1)
+
+    # Drawing the shapes
+    sg.drawSceneGraphNode(tree, pipeline, "model")
+
+
+# Main function
 if __name__ == "__main__":
 
     # Initialize glfw
@@ -270,7 +378,7 @@ if __name__ == "__main__":
     width = 600
     height = 600
 
-    window = glfw.create_window(width, height, "Invisible tree", None, None)
+    window = glfw.create_window(width, height, "Tree", None, None)
 
     if not window:
         glfw.terminate()
@@ -296,7 +404,7 @@ if __name__ == "__main__":
 
     # Creating shapes on GPU memory
     gpuAxis = es.toGPUShape(bs.createAxis())
-    treeGraph = createTree(0, "F[URF]F[RF]F[DLF]F")
+    treeGraph = createTree(2, "F[URF]F[RF]F[DLF]F")
 
     # Setting up the projection
     projection = tr.perspective(45, float(width)/float(height), 0.1, 100)
@@ -328,36 +436,28 @@ if __name__ == "__main__":
         
         # Using the lighting shader program
         glUseProgram(lightingPipeline.shaderProgram)
-
-        # Setting all uniform shader variables
-        
-        # White light in all components: ambient, diffuse and specular.
-        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
-        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
-        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
-
-        # Object is barely visible at only ambient and brighter for the diffuse component.
-        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ka"), 0.3, 0.3, 0.3)
-        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Kd"), 0.5, 0.5, 0.5)
-        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ks"), 0.1, 0.1, 0.1)
-
-        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "lightPosition"), 5, 5, 5)
-        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "viewPosition"), viewPos[0], viewPos[1], viewPos[2])
-
-        # In my humble opinion, natural wood isn't shiny at all
-        glUniform1ui(glGetUniformLocation(lightingPipeline.shaderProgram, "shininess"), 1)
-
-        # Finishing the lighting configuration
-        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "constantAttenuation"), 0.0001)
-        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "linearAttenuation"), 0.03)
-        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "quadraticAttenuation"), 0.01)
-
         glUniformMatrix4fv(glGetUniformLocation(lightingPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
         glUniformMatrix4fv(glGetUniformLocation(lightingPipeline.shaderProgram, "view"), 1, GL_TRUE, view)
         glUniformMatrix4fv(glGetUniformLocation(lightingPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.identity())
 
-        # Drawing the shapes
-        sg.drawSceneGraphNode(treeGraph, lightingPipeline, "model")
+        # Setting all uniform shader variables
+        
+        # White light in all components: ambient, diffuse and specular
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
+
+        # Constants of the light
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "constantAttenuation"), 0.0001)
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "linearAttenuation"), 0.03)
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "quadraticAttenuation"), 0.01)
+
+        # Finishing the lighting configuration
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "lightPosition"), 5, 5, 5)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "viewPosition"), viewPos[0], viewPos[1], viewPos[2])
+
+        # Drawing the shapes according to material properties
+        drawTree(treeGraph, lightingPipeline)
 
         # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
         glfw.swap_buffers(window)
