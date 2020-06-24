5b9cf5a53ed0d300ee9584acdc14c8e56299fb57
alec280 <alec280@gmail.com>
2020-06-23 23:00:18 -0400

    Se añade el mapa de calor en el suelo.

--- tarea3B/hotel-viewer.py
+++ tarea3B/hotel-viewer.py
@@ -39,6 +39,12 @@ except:
     sys.exit()
 
 solution = np.load(solution_name)
+shapeSol = solution.shape
+
+# Checks minimal and maximal values for the heat map
+minval = np.min(solution[np.nonzero(solution)])
+maxval = np.max(solution)
+midval = (maxval + minval) / 2
 
 
 # Class that stores the application control
@@ -52,6 +58,7 @@ class Controller:
 
 # Global controller that communicates with the callback function
 controller = Controller()
+
 
 # Helps to control the camera by snapping it to these degrees
 SNAP_ANGLES = np.array([0, np.pi / 4, np.pi / 2, 3 * np.pi / 4, np.pi, 
@@ -104,10 +111,24 @@ def on_key(window, key, scancode, action, mods):
 
 
 # Colorates the heat map with 3 colors
-def colorMap(value, active):
-    if not active:
-        return [0.75, 0.75, 0.75]
-    return [0, 0, 0]
+def colorMap(i, j):
+
+    # Getting the temperature
+    x = min(int(i / 0.1 + 0.0001), shapeSol[0] - 1)
+    y = min(int(j / 0.1 + 0.0001), shapeSol[1] - 1)
+    temperature = solution[x, y]
+
+    colorMin = np.array([0, 0, 1])
+    colorMid = np.array([1, 1, 0])
+    colorMax = np.array([1, 0, 0])
+
+    # Assigning the colors
+    if minval <= temperature <= midval:
+        ratio = (temperature - minval) / (midval - minval)
+        return list(colorMin * (1 - ratio) + colorMid * ratio)
+    else:
+        ratio = (temperature - midval) / (maxval - midval)
+        return list(colorMid * (1 - ratio) + colorMax * ratio)
 
 
 # Returns a list of the key points of the geometry
@@ -163,16 +184,59 @@ def keyPoints():
     return pts
 
 
-# Shape of the floor and roof
-def blueprint(floor = False):
+# Shape of the floor
+def createFloor():
     global data
 
-    normal = [0, 0, 1] if floor else [0, 0, -1]
+    # Generate a terrain with many samples between the limits of the map
+    xs = np.ogrid[0:5 * data["L"] + 4 * data["W"]:160j]
+    ys = np.ogrid[0:data["P"] + data["W"] + data["D"]:160j]
+
+    xSize = len(xs)
+    ySize = len(ys)
+
+    # Defining the location and colors of each vertex of the shape
+    vertices = []
+    indices = []
+
+    # Generating a vertex for each sample x, y, and assigning a color
+    for i in range(xSize):
+        for j in range(ySize):
+            x = xs[i]
+            y = ys[j]
+
+            vertices += [x, y, 0] + colorMap(x, y)
+
+    # The previous loops generates full columns j-y and then move to
+    # the next i-x. Hence, the index for each vertex i,j can be computed as
+    index = lambda i, j: i * len(ys) + j 
     
+    # We generate quads for each cell connecting 4 neighbor vertices
+    for i in range(len(xs) - 1):
+        for j in range(len(ys) - 1):
+
+            # Getting indices for all vertices in this quad
+            isw = index(i,j)
+            ise = index(i+1,j)
+            ine = index(i+1,j+1)
+            inw = index(i,j+1)
+
+            # Adding this cell's quad as 2 triangles
+            indices += [
+                isw, ise, ine,
+                ine, inw, isw
+            ]
+
+    return bs.Shape(vertices, indices)
+
+
+# Shape of the roof
+def createRoof():
+
     # Defining the location and colors of each vertex of the shape
     vertices = []
     for vertex in keyPoints():
-        vertices += vertex + colorMap(vertex, floor) + normal 
+        vertices += vertex + [0.75, 0.75, 0.75] + [0, 0, -1]
 
     # Defining connections among vertices
     indices = [
@@ -190,188 +254,105 @@ def blueprint(floor = False):
 
 # Shape of the walls
 def createWalls():
-    global data
 
+    # Defining the location and colors of each vertex of the shape
+    vertices = []
     color = [0.75, 0.75, 0.75]
 
-    # Defining the location and colors of each vertex  of the shape
+    # Points to use
     pts = keyPoints()
     hpts = list(map(lambda x: [x[0], x[1], 1], pts.copy()))
 
-    vertices = []
-    vertices += pts[0] + color + [0, 1, 0]  #0
-    vertices += hpts[0] + color + [0, 1, 0]
-    vertices += pts[1] + color + [0, 1, 0]
-    vertices += hpts[1] + color + [0, 1, 0]
-    vertices += pts[2] + color + [0, 1, 0]
-    vertices += hpts[2] + color + [0, 1, 0]  #5
-    vertices += pts[3] + color + [0, 1, 0]
-    vertices += hpts[3] + color + [0, 1, 0]
-    vertices += pts[0] + color + [1, 0, 0]
-    vertices += hpts[0] + color + [1, 0, 0]
-    vertices += pts[4] + color + [1, 0, 0]  # 10
-    vertices += hpts[4] + color + [1, 0, 0]
-    vertices += pts[3] + color + [-1, 0, 0]
-    vertices += hpts[3] + color + [-1, 0, 0]
-    vertices += pts[33] + color + [-1, 0, 0]
-    vertices += hpts[33] + color + [-1, 0, 0]  #15
-    vertices += pts[14] + color + [1, 0, 0]
-    vertices += hpts[14] + color + [1, 0, 0]
-    vertices += pts[24] + color + [1, 0, 0]
-    vertices += hpts[24] + color + [1, 0, 0]
-    vertices += pts[6] + color + [-1, 0, 0]  #20
-    vertices += hpts[6] + color + [-1, 0, 0]
-    vertices += pts[25] + color + [-1, 0, 0]
-    vertices += hpts[25] + color + [-1, 0, 0]
-    vertices += pts[8] + color + [-1, 0, 0]
-    vertices += hpts[8] + color + [-1, 0, 0]  #25
-    vertices += pts[27] + color + [-1, 0, 0]
-    vertices += hpts[27] + color + [-1, 0, 0]
-    vertices += pts[10] + color + [-1, 0, 0]
-    vertices += hpts[10] + color + [-1, 0, 0]
-    vertices += pts[29] + color + [-1, 0, 0]  #30
-    vertices += hpts[29] + color + [-1, 0, 0]
-    vertices += pts[12] + color + [-1, 0, 0]
-    vertices += hpts[12] + color + [-1, 0, 0]
-    vertices += pts[31] + color + [-1, 0, 0]
-    vertices += hpts[31] + color + [-1, 0, 0]  #35
-    vertices += pts[16] + color + [1, 0, 0]
-    vertices += hpts[16] + color + [1, 0, 0]
-    vertices += pts[26] + color + [1, 0, 0]
-    vertices += hpts[26] + color + [1, 0, 0]
-    vertices += pts[18] + color + [1, 0, 0]  #40
-    vertices += hpts[18] + color + [1, 0, 0]
-    vertices += pts[28] + color + [1, 0, 0]
-    vertices += hpts[28] + color + [1, 0, 0]
-    vertices += pts[20] + color + [1, 0, 0]
-    vertices += hpts[20] + color + [1, 0, 0] #45
-    vertices += pts[30] + color + [1, 0, 0]
-    vertices += hpts[30] + color + [1, 0, 0]
-    vertices += pts[22] + color + [1, 0, 0]
-    vertices += hpts[22] + color + [1, 0, 0]
-    vertices += pts[32] + color + [1, 0, 0]  #50
-    vertices += hpts[32] + color + [1, 0, 0]
-    vertices += pts[4] + color + [0, -1, 0]
-    vertices += hpts[4] + color + [0, -1, 0]
-    vertices += pts[5] + color + [0, -1, 0]
-    vertices += hpts[5] + color + [0, -1, 0]  #55
-    vertices += pts[6] + color + [0, -1, 0]
-    vertices += hpts[6] + color + [0, -1, 0]
-    vertices += pts[7] + color + [0, -1, 0]
-    vertices += hpts[7] + color + [0, -1, 0]
-    vertices += pts[8] + color + [0, -1, 0]  #60
-    vertices += hpts[8] + color + [0, -1, 0]
-    vertices += pts[9] + color + [0, -1, 0]
-    vertices += hpts[9] + color + [0, -1, 0]
-    vertices += pts[10] + color + [0, -1, 0]
-    vertices += hpts[10] + color + [0, -1, 0]  #65
-    vertices += pts[11] + color + [0, -1, 0]
-    vertices += hpts[11] + color + [0, -1, 0]
-    vertices += pts[12] + color + [0, -1, 0]
-    vertices += hpts[12] + color + [0, -1, 0]
-    vertices += pts[13] + color + [0, -1, 0]  #70
-    vertices += hpts[13] + color + [0, -1, 0]
-    vertices += pts[14] + color + [0, 1, 0]
-    vertices += hpts[14] + color + [0, 1, 0]
-    vertices += pts[15] + color + [0, 1, 0]
-    vertices += hpts[15] + color + [0, 1, 0]  #75  
-    vertices += pts[16] + color + [0, 1, 0]
-    vertices += hpts[16] + color + [0, 1, 0]
-    vertices += pts[17] + color + [0, 1, 0]
-    vertices += hpts[17] + color + [0, 1, 0]
-    vertices += pts[18] + color + [0, 1, 0]  #80
-    vertices += hpts[18] + color + [0, 1, 0]
-    vertices += pts[19] + color + [0, 1, 0]
-    vertices += hpts[19] + color + [0, 1, 0]
-    vertices += pts[20] + color + [0, 1, 0]
-    vertices += hpts[20] + color + [0, 1, 0]  #85
-    vertices += pts[21] + color + [0, 1, 0]
-    vertices += hpts[21] + color + [0, 1, 0]
-    vertices += pts[22] + color + [0, 1, 0]
-    vertices += hpts[22] + color + [0, 1, 0]
-    vertices += pts[23] + color + [0, 1, 0]  #90
-    vertices += hpts[23] + color + [0, 1, 0]
-    vertices += pts[5] + color + [1, 0, 0]
-    vertices += hpts[5] + color + [1, 0, 0]
-    vertices += pts[15] + color + [1, 0, 0]
-    vertices += hpts[15] + color + [1, 0, 0]  #95
-    vertices += pts[7] + color + [1, 0, 0]
-    vertices += hpts[7] + color + [1, 0, 0]
-    vertices += pts[17] + color + [1, 0, 0]
-    vertices += hpts[17] + color + [1, 0, 0]
-    vertices += pts[9] + color + [1, 0, 0]  #100
-    vertices += hpts[9] + color + [1, 0, 0]
-    vertices += pts[19] + color + [1, 0, 0]
-    vertices += hpts[19] + color + [1, 0, 0]
-    vertices += pts[11] + color + [1, 0, 0]
-    vertices += hpts[11] + color + [1, 0, 0]  #105
-    vertices += pts[21] + color + [1, 0, 0]
-    vertices += hpts[21] + color + [1, 0, 0]
-    vertices += pts[13] + color + [1, 0, 0]
-    vertices += hpts[13] + color + [1, 0, 0]
-    vertices += pts[23] + color + [1, 0, 0]  #110
-    vertices += hpts[23] + color + [1, 0, 0]
+    # Drawing the walls
+    vertices += pts[0] + color + [0, 1, 0] + hpts[0] + color + [0, 1, 0]
+    vertices += pts[1] + color + [0, 1, 0] + hpts[1] + color + [0, 1, 0]
+    vertices += pts[2] + color + [0, 1, 0] + hpts[2] + color + [0, 1, 0]
+    vertices += pts[3] + color + [0, 1, 0] + hpts[3] + color + [0, 1, 0]
+    vertices += pts[0] + color + [1, 0, 0] + hpts[0] + color + [1, 0, 0]
+    vertices += pts[4] + color + [1, 0, 0] + hpts[4] + color + [1, 0, 0]
+    vertices += pts[3] + color + [-1, 0, 0] + hpts[3] + color + [-1, 0, 0]
+    vertices += pts[33] + color + [-1, 0, 0] + hpts[33] + color + [-1, 0, 0]
+    vertices += pts[14] + color + [1, 0, 0] + hpts[14] + color + [1, 0, 0]
+    vertices += pts[24] + color + [1, 0, 0] + hpts[24] + color + [1, 0, 0]
+    vertices += pts[6] + color + [-1, 0, 0] + hpts[6] + color + [-1, 0, 0]
+    vertices += pts[25] + color + [-1, 0, 0] + hpts[25] + color + [-1, 0, 0]
+    vertices += pts[8] + color + [-1, 0, 0] + hpts[8] + color + [-1, 0, 0]
+    vertices += pts[27] + color + [-1, 0, 0] + hpts[27] + color + [-1, 0, 0]
+    vertices += pts[10] + color + [-1, 0, 0] + hpts[10] + color + [-1, 0, 0]
+    vertices += pts[29] + color + [-1, 0, 0] + hpts[29] + color + [-1, 0, 0]
+    vertices += pts[12] + color + [-1, 0, 0] + hpts[12] + color + [-1, 0, 0]
+    vertices += pts[31] + color + [-1, 0, 0] + hpts[31] + color + [-1, 0, 0]
+    vertices += pts[16] + color + [1, 0, 0] + hpts[16] + color + [1, 0, 0]
+    vertices += pts[26] + color + [1, 0, 0] + hpts[26] + color + [1, 0, 0]
+    vertices += pts[18] + color + [1, 0, 0] + hpts[18] + color + [1, 0, 0]
+    vertices += pts[28] + color + [1, 0, 0] + hpts[28] + color + [1, 0, 0]
+    vertices += pts[20] + color + [1, 0, 0] + hpts[20] + color + [1, 0, 0]
+    vertices += pts[30] + color + [1, 0, 0] + hpts[30] + color + [1, 0, 0]
+    vertices += pts[22] + color + [1, 0, 0] + hpts[22] + color + [1, 0, 0]
+    vertices += pts[32] + color + [1, 0, 0] + hpts[32] + color + [1, 0, 0]
+    vertices += pts[4] + color + [0, -1, 0] + hpts[4] + color + [0, -1, 0]
+    vertices += pts[5] + color + [0, -1, 0] + hpts[5] + color + [0, -1, 0]
+    vertices += pts[6] + color + [0, -1, 0] + hpts[6] + color + [0, -1, 0]
+    vertices += pts[7] + color + [0, -1, 0] + hpts[7] + color + [0, -1, 0]
+    vertices += pts[8] + color + [0, -1, 0] + hpts[8] + color + [0, -1, 0]
+    vertices += pts[9] + color + [0, -1, 0] + hpts[9] + color + [0, -1, 0]
+    vertices += pts[10] + color + [0, -1, 0] + hpts[10] + color + [0, -1, 0]
+    vertices += pts[11] + color + [0, -1, 0] + hpts[11] + color + [0, -1, 0]
+    vertices += pts[12] + color + [0, -1, 0] + hpts[12] + color + [0, -1, 0]
+    vertices += pts[13] + color + [0, -1, 0] + hpts[13] + color + [0, -1, 0]
+    vertices += pts[14] + color + [0, 1, 0] + hpts[14] + color + [0, 1, 0]
+    vertices += pts[15] + color + [0, 1, 0] + hpts[15] + color + [0, 1, 0]
+    vertices += pts[16] + color + [0, 1, 0] + hpts[16] + color + [0, 1, 0]
+    vertices += pts[17] + color + [0, 1, 0] + hpts[17] + color + [0, 1, 0]
+    vertices += pts[18] + color + [0, 1, 0] + hpts[18] + color + [0, 1, 0]
+    vertices += pts[19] + color + [0, 1, 0] + hpts[19] + color + [0, 1, 0]
+    vertices += pts[20] + color + [0, 1, 0] + hpts[20] + color + [0, 1, 0]
+    vertices += pts[21] + color + [0, 1, 0] + hpts[21] + color + [0, 1, 0]
+    vertices += pts[22] + color + [0, 1, 0] + hpts[22] + color + [0, 1, 0]
+    vertices += pts[23] + color + [0, 1, 0] + hpts[23] + color + [0, 1, 0]
+    vertices += pts[5] + color + [1, 0, 0] + hpts[5] + color + [1, 0, 0]
+    vertices += pts[15] + color + [1, 0, 0] + hpts[15] + color + [1, 0, 0]
+    vertices += pts[7] + color + [1, 0, 0] + hpts[7] + color + [1, 0, 0]
+    vertices += pts[17] + color + [1, 0, 0] + hpts[17] + color + [1, 0, 0]
+    vertices += pts[9] + color + [1, 0, 0] + hpts[9] + color + [1, 0, 0]
+    vertices += pts[19] + color + [1, 0, 0] + hpts[19] + color + [1, 0, 0]
+    vertices += pts[11] + color + [1, 0, 0] + hpts[11] + color + [1, 0, 0]
+    vertices += pts[21] + color + [1, 0, 0] + hpts[21] + color + [1, 0, 0]
+    vertices += pts[13] + color + [1, 0, 0] + hpts[13] + color + [1, 0, 0]
+    vertices += pts[23] + color + [1, 0, 0] + hpts[23] + color + [1, 0, 0]
+
+    # Drawing the heater
+    vertices += pts[1] + [1, 0, 0] + [0, 1, 0] + hpts[1] + [1, 0, 0] + [0, 1, 0]
+    vertices += pts[2] + [1, 0, 0] + [0, 1, 0] + hpts[2] + [1, 0, 0] + [0, 1, 0]
+
+    # Drawing the bottom of the windows
+    vertices += pts[24] + color + [0, -1, 0] + hpts[24][:2] + [0.3] + color + [0, -1, 0]
+    vertices += pts[25] + color + [0, -1, 0] + hpts[25][:2] + [0.3]  + color + [0, -1, 0]
+    vertices += pts[26] + color + [0, -1, 0] + hpts[26][:2] + [0.3] + color + [0, -1, 0]
+    vertices += pts[27] + color + [0, -1, 0] + hpts[27][:2] + [0.3]  + color + [0, -1, 0]
+    vertices += pts[28] + color + [0, -1, 0] + hpts[28][:2] + [0.3] + color + [0, -1, 0]
+    vertices += pts[29] + color + [0, -1, 0] + hpts[29][:2] + [0.3]  + color + [0, -1, 0]
+    vertices += pts[30] + color + [0, -1, 0] + hpts[30][:2] + [0.3] + color + [0, -1, 0]
+    vertices += pts[31] + color + [0, -1, 0] + hpts[31][:2] + [0.3]  + color + [0, -1, 0]
+    vertices += pts[32] + color + [0, -1, 0] + hpts[32][:2] + [0.3] + color + [0, -1, 0]
+    vertices += pts[33] + color + [0, -1, 0] + hpts[33][:2] + [0.3]  + color + [0, -1, 0]
 
     # Defining connections among vertices
     indices = [
-           0,  1,  2, 
-           1,  3,  2,
-           2,  3,  4,
-           3,  5,  4,
-           4,  5,  6,
-           5,  7,  6,
-           8, 10,  9,
-          10,  9, 11,
-          13, 14, 12,
-          13, 15, 14,
-          16, 18, 17,
-          17, 18, 19,
-          21, 22, 20,
-          21, 23, 22,
-          25, 26, 24,
-          25, 27, 26,
-          29, 30, 28,
-          29, 31, 30,
-          33, 34, 32,
-          33, 35, 34,
-          36, 38, 37,
-          37, 38, 39,
-          40, 42, 41,
-          41, 42, 43,
-          44, 46, 45,
-          45, 46, 47,
-          48, 50, 49,
-          49, 50, 51,
-          52, 54, 53,
-          53, 54, 55,
-          56, 58, 57,
-          57, 58, 59,
-          60, 62, 61,
-          61, 62, 63,
-          64, 66, 65,
-          65, 66, 67,
-          68, 70, 69,
-          69, 70, 71,
-          75, 74, 72,
-          75, 72, 73,
-          79, 78, 76,
-          79, 76, 77,
-          83, 82, 80,
-          83, 80, 81,
-          87, 86, 84,
-          87, 84, 85,
-          91, 90, 88,
-          91, 88, 89,
-          93, 92, 94,
-          93, 94, 95,
-          97, 96, 98,
-          97, 98, 99,
-         101,100,102,
-         101,102,103,
-         105,104,106,
-         105,106,107,
-         109,108,110,
-         109,110,111,
+           0,  1,  2,  1,  3,  2,  4,  5,  6,  5,  7,  6,  8, 10,  9,
+          10,  9, 11, 13, 14, 12, 13, 15, 14, 16, 18, 17, 17, 18, 19,
+          21, 22, 20, 21, 23, 22, 25, 26, 24, 25, 27, 26, 29, 30, 28,
+          29, 31, 30, 33, 34, 32, 33, 35, 34, 36, 38, 37, 37, 38, 39,
+          40, 42, 41, 41, 42, 43, 44, 46, 45, 45, 46, 47, 48, 50, 49,
+          49, 50, 51, 52, 54, 53, 53, 54, 55, 56, 58, 57, 57, 58, 59,
+          60, 62, 61, 61, 62, 63, 64, 66, 65, 65, 66, 67, 68, 70, 69,
+          69, 70, 71, 75, 74, 72, 75, 72, 73, 79, 78, 76, 79, 76, 77,
+          83, 82, 80, 83, 80, 81, 87, 86, 84, 87, 84, 85, 91, 90, 88,
+          91, 88, 89, 93, 92, 94, 93, 94, 95, 97, 96, 98, 97, 98, 99,
+         101,100,102,101,102,103,105,104,106,105,106,107,109,108,110,
+         109,110,111,112,113,114,113,115,114,117,116,118,117,118,119,
+         121,120,122,121,122,123,125,124,126,125,126,127,129,128,130,
+         129,130,131,133,132,134,133,134,135,
     ]
     return bs.Shape(vertices, indices)
 
@@ -417,8 +398,8 @@ if __name__ == "__main__":
     #pipeline = ls.SimpleFlatShaderProgram()
     pipeline = ls.SimpleGouraudShaderProgram()
     #pipeline = ls.SimplePhongShaderProgram()
+    simplePipeline = es.SimpleModelViewProjectionShaderProgram()
 
-    # Telling OpenGL to use our shader program
     glUseProgram(pipeline.shaderProgram)
 
     # Setting up the clear screen color
@@ -431,10 +412,10 @@ if __name__ == "__main__":
     # Creating the graphs
     roofGraph = sg.SceneGraphNode("roof")
     roofGraph.transform = tr.translate(0, 0, 1)
-    roofGraph.childs += [es.toGPUShape(blueprint())]
+    roofGraph.childs += [es.toGPUShape(createRoof())]
 
     floorGraph = sg.SceneGraphNode("floor")
-    floorGraph.childs += [es.toGPUShape(blueprint(True))]
+    floorGraph.childs += [es.toGPUShape(createFloor())]
 
     wallGraph = sg.SceneGraphNode("wall")
     wallGraph.childs += [es.toGPUShape(createWalls())]
@@ -445,6 +426,8 @@ if __name__ == "__main__":
     # Light position
     lgX = 1.5 * data["L"] + data["W"]
     lgY = 1.5 * (data["P"] + data["W"] + data["D"])
+
+    print("You can move with the arrow keys.")
 
     while not glfw.window_should_close(window):
         # Using GLFW to check for input events
@@ -461,6 +444,13 @@ if __name__ == "__main__":
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
         else:
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+        
+        # The floow is drawn without light effects
+        glUseProgram(simplePipeline.shaderProgram)
+        glUniformMatrix4fv(glGetUniformLocation(simplePipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(simplePipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+        glUniformMatrix4fv(glGetUniformLocation(simplePipeline.shaderProgram, "model"), 1, GL_TRUE, tr.identity())
+        sg.drawSceneGraphNode(floorGraph, simplePipeline, "model")
 
         # Using the lighting shader program
         glUseProgram(pipeline.shaderProgram)
@@ -486,7 +476,6 @@ if __name__ == "__main__":
         # Drawing the graphs
         sg.drawSceneGraphNode(wallGraph, pipeline, "model")
         sg.drawSceneGraphNode(roofGraph, pipeline, "model")
-        sg.drawSceneGraphNode(floorGraph, pipeline, "model")
 
         # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
         glfw.swap_buffers(window)
